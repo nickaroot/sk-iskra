43933b38f3df7b61266e30063307f0759185d824
Grigory Kuznetsov <gakuznetsov@alfabank.ru>
2017-11-04 18:17:54 +0300

    add propTypes

--- spark-ui/src/containers/App.js
+++ spark-ui/src/containers/App.js
@@ -1,10 +1,18 @@
 import React from 'react';
 import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
 
 import Header from '../components/header/Header';
 import Nav from '../components/nav/Nav';
 
 class App extends React.Component {
+  static propTypes = {
+    deviceId: PropTypes.string,
+    history: PropTypes.shape({
+      push: PropTypes.func
+    })
+  }
+
   componentWillMount() {
     if (!this.props.deviceId) {
       this.props.history.push('/auth');
@@ -23,7 +31,7 @@ class App extends React.Component {
 
 const mapStateToProps = state => {
   return {
-    deviceId : state.app.deviceId
+    deviceId: state.app.deviceId
   };
 };
 
