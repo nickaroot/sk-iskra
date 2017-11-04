42ac99abc9c4b2752d0ed3f36b33f9693895e832
Grigory Kuznetsov <gakuznetsov@alfabank.ru>
2017-11-04 14:43:23 +0300

    init ui

---
+++ spark-ui/src/components/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+export default class App extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  componentDidMount() {
+  }
+
+  render() {
+    return (
+      <div>Test</div>
+    );
+  }
+}
