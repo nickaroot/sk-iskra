c91f121d413ece9a05a89f026aa01ca6987ebb9f
Grigory Kuznetsov <gakuznetsov@alfabank.ru>
2017-11-04 14:43:23 +0300

    init ui

---
+++ spark-ui/webpack.config.js
@@ -0,0 +1,61 @@
+const { resolve } = require('path');
+
+const webpack = require('webpack');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+
+const config = {
+  entry: [
+    './src/index.js'
+  ],
+
+  output: {
+    filename: 'bundle.js',
+    path: resolve(__dirname, './dist'),
+    publicPath: '/',
+  },
+
+  context: __dirname,
+
+  devServer: {
+    contentBase: resolve(__dirname, './src'),
+    publicPath: '/',
+    port: 9010
+  },
+
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        loaders: [
+          'babel-loader',
+        ],
+        exclude: /node_modules/,
+      },
+      {
+		    test: /\.scss/,
+		    loader: 'style-loader!css-loader?modules&camelCase&importLoaders=1&localIdentName=[name]__[local]___[hash:base64:5]!postcss-loader',
+      },
+      { test: /\.(png|jpg)$/, use: 'url-loader?limit=15000' },
+      { test: /\.eot(\?v=\d+.\d+.\d+)?$/, use: 'file-loader' },
+      { test: /\.woff(2)?(\?v=[0-9]\.[0-9]\.[0-9])?$/, use: 'url-loader?limit=10000&mimetype=application/font-woff' },
+      { test: /\.[ot]tf(\?v=\d+.\d+.\d+)?$/, use: 'url-loader?limit=10000&mimetype=application/octet-stream' },
+      { test: /\.svg(\?v=\d+\.\d+\.\d+)?$/, use: 'url-loader?limit=10000&mimetype=image/svg+xml' },
+    ]
+  },
+
+  plugins: [
+    new webpack.LoaderOptionsPlugin({
+      test: /\.js$/,
+      options: {
+        eslint: {
+          configFile: resolve(__dirname, '.eslintrc'),
+          cache: false,
+        }
+      },
+    }),
+    new webpack.optimize.ModuleConcatenationPlugin(),
+    new ExtractTextPlugin({ filename: 'style.css', disable: false, allChunks: true }),
+  ],
+};
+
+module.exports = config;
