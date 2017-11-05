cbc98b09c7ed1342dc867044ac32e23ddae18d74
nickaroot <funtrum@icloud.com>
2017-11-05 17:41:06 +0300

    Final colors change

--- spark-ui/src/components/nav/Nav.js
+++ spark-ui/src/components/nav/Nav.js
@@ -2,11 +2,14 @@ import React from 'react';
 import { Tabs, Tab } from 'material-ui/Tabs';
 import Personal from '../personal/Personal';
 import Device from '../device/Device';
+import styles from './styles.scss';
+
+// d8af0a
 
 export default class Nav extends React.Component {
   render() {
     return (
-      <Tabs>
+      <Tabs inkBarStyle={{"backgroundColor": "#d8af0a"}}>
         <Tab label="Браслет" style={{"backgroundColor": "#0070c0"}}>
           <Device />
         </Tab>
