6c512f787b5dc3ab74dac9cf9bf4d1c16685786f
777 <a777@MacBook-Pro-777.local>
2017-11-04 18:11:20 -0700

    add auth

--- spark-ui/src/containers/Auth.js
+++ spark-ui/src/containers/Auth.js
@@ -1,9 +1,16 @@
 import React from 'react';
+import TextField from 'material-ui/TextField';
+import styles from './styles.scss';
 
 export default class Auth extends React.Component {
   render() {
     return (
-      <div>Auth</div>
+      <div className={styles.textField}>
+        <TextField
+          hintText="id браслета"
+          floatingLabelText="Введите id браслета"
+        />
+      </div>
     );
   }
 }
