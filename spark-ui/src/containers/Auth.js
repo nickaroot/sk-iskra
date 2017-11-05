ac52946e62e89c0c951a2589fbc897a9f14913a9
777 <a777@MacBook-Pro-777.local>
2017-11-04 19:21:03 -0700

    add login button disabled

--- spark-ui/src/containers/Auth.js
+++ spark-ui/src/containers/Auth.js
@@ -1,14 +1,56 @@
 import React from 'react';
 import TextField from 'material-ui/TextField';
+import RaisedButton from 'material-ui/RaisedButton';
+import PropTypes from 'prop-types';
+
 import styles from './styles.scss';
 
 export default class Auth extends React.Component {
+  static propTypes = {
+    history: PropTypes.shape({
+      push: PropTypes.func
+    })
+  }
+
+  constructor(props) {
+    super(props);
+
+    this.state = { id: '' };
+  }
+
+  handleIdChange = (evt, newValue) => {
+    this.setState({
+      id: newValue
+    });
+  }
+
+  handleClick = () => {
+    this.props.history.push('/');
+  }
+
   render() {
+    const { id } = this.state;
+
+    let isDisabled = true;
+    if (id.length > 0) {
+      isDisabled = false;
+    }
+
     return (
       <div className={styles.textField}>
         <TextField
           hintText="id браслета"
           floatingLabelText="Введите id браслета"
+          onChange={this.handleIdChange}
+        />
+        <RaisedButton
+          disabled={isDisabled}
+          label="Войти"
+          primary
+          style={{
+            display: 'block'
+          }}
+          onClick={this.handleClick}
         />
       </div>
     );
